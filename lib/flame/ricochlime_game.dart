e8e92f3f132cb7aa78435aefb338056b7326cc6e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-08-01 22:44:57 +0100

    feat: move player with drag gesture

--- lib/flame/ricochlime_game.dart
+++ lib/flame/ricochlime_game.dart
@@ -1,18 +1,25 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:ricochlime/flame/components/player.dart';
 
-class RicochlimeGame extends FlameGame {
+class RicochlimeGame extends FlameGame with PanDetector {
+  late Player player;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
-    add(
-      Player()
+    player = Player()
         ..position = size / 2
         ..width = 50
         ..height = 100
-        ..anchor = Anchor.center,
-    );
+        ..anchor = Anchor.center;
+    add(player);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    player.position.add(info.delta.game);
   }
 }
