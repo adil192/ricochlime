13ac840ce8f1bbab7bb4b12febfde02cd9cbd42a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-08-03 17:45:57 +0100

    feat: add collisions to bullets

--- lib/flame/components/bullet.dart
+++ lib/flame/components/bullet.dart
@@ -1,23 +1,29 @@
 import 'dart:ui';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
+import 'package:ricochlime/flame/ricochlime_game.dart';
 
-class Bullet extends PositionComponent {
+class Bullet extends PositionComponent with
+    HasGameRef<RicochlimeGame> {
   static const radius = 2.0;
 
   late final _paint = Paint()
     ..color = const Color(0xFFFFFFFF);
 
-  double speed = radius * 50;
+  double speed = radius * 10;
   Vector2 direction;
+
+  RaycastResult<ShapeHitbox> raycastResult = RaycastResult();
 
   Bullet({
     required super.position,
     required this.direction,
-  }) : super(
-    size: Vector2.all(radius * 2),
-    anchor: Anchor.center,
-  );
+  }): super(
+        size: Vector2.all(radius * 2),
+        anchor: Anchor.center,
+      );
 
   @override
   Future<void> onLoad() async {
@@ -27,7 +33,24 @@ class Bullet extends PositionComponent {
   @override
   void update(double dt) {
     super.update(dt);
+    handleCollisions(dt);
     position += direction * speed * dt;
+  }
+
+  void handleCollisions(double dt) {
+    gameRef.collisionDetection.raycast(
+      Ray2(
+        origin: position,
+        direction: direction,
+      ),
+      maxDistance: speed * dt + radius,
+      out: raycastResult,
+    );
+    final reflectionRay = raycastResult.reflectionRay;
+    if (reflectionRay == null) {
+      return;
+    }
+    direction = reflectionRay.direction;
   }
 
   @override
