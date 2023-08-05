fe2dc6489268fb8cc6dfa099472a28b9b77b539e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-08-05 04:39:06 +0100

    feat: add animation from home page to play page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:ricochlime/pages/play.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,13 +11,24 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Ricochlime'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.push('/play');
-          },
-          child: const Text('Play'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: OpenContainer(
+              closedBuilder: (context, openContainer) => ElevatedButton(
+                onPressed: openContainer,
+                child: const Text('Play'),
+              ),
+              closedColor: Colors.transparent,
+              closedElevation: 0,
+              closedShape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+              ),
+              openBuilder: (context, closeContainer) => PlayPage(),
+            ),
+          ),
+        ],
       ),
     );
   }
