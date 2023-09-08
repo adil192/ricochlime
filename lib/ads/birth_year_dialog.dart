c0481e3a7c4197be426733cc5e528baeb31abc04
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-09-08 07:49:06 +0100

    feat: make birth year dialog more retro

--- lib/ads/birth_year_dialog.dart
+++ lib/ads/birth_year_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nes_ui/nes_ui.dart';
 import 'package:ricochlime/i18n/strings.g.dart';
 import 'package:ricochlime/utils/prefs.dart';
 
@@ -23,47 +24,65 @@ class _BirthYearDialogState extends State<BirthYearDialog> {
   @override
   Widget build(BuildContext context) {
     final currentYear = DateTime.now().year;
-    return AlertDialog(
-      title: Text(t.birthYear.yourBirthYear),
-      content: SizedBox(
-        width: 300,
-        height: 300,
-        child: Column(
-          children: [
-            Text(t.birthYear.reason),
-            Expanded(
-              child: StatefulBuilder(
-                builder: (context, setState) {
-                  return YearPicker(
-                    firstDate: DateTime(1900),
-                    lastDate: DateTime(currentYear),
-                    initialDate: DateTime(currentYear),
-                    selectedDate: selectedDate,
-                    onChanged: (value) {
-                      setState(() => selectedDate = value);
-                    },
-                  );
-                },
+    final colorScheme = Theme.of(context).colorScheme;
+    return Center(
+      child: NesContainer(
+        width: 300 + 32 * 2,
+        padding: const EdgeInsets.all(32),
+        backgroundColor: colorScheme.surface,
+        child: Material(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                t.birthYear.yourBirthYear,
+                style: const TextStyle(
+                  fontSize: kToolbarHeight / 2,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              Text(t.birthYear.reason),
+              SizedBox(
+                height: 300,
+                child: StatefulBuilder(
+                  builder: (context, setState) {
+                    return YearPicker(
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime(currentYear),
+                      initialDate: DateTime(currentYear),
+                      selectedDate: selectedDate,
+                      onChanged: (value) {
+                        setState(() => selectedDate = value);
+                      },
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 16),
+              Wrap(
+                children: [
+                  if (widget.dismissible) NesButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+                    type: NesButtonType.warning,
+                    child: Text(t.common.cancel),
+                  ),
+                  const SizedBox(width: 16),
+                  NesButton(
+                    onPressed: () {
+                      Prefs.birthYear.value = selectedDate.year;
+                      context.pop();
+                    },
+                    type: NesButtonType.primary,
+                    child: Text(t.common.ok),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      actions: [
-        if (widget.dismissible) TextButton(
-          onPressed: () {
-            context.pop();
-          },
-          child: Text(t.common.cancel),
-        ),
-        TextButton(
-          onPressed: () {
-            Prefs.birthYear.value = selectedDate.year;
-            context.pop();
-          },
-          child: Text(t.common.ok),
-        ),
-      ],
     );
   }
 }
