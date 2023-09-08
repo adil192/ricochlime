670a85c6b87653798e0602a7f9f5819c36a6f0b5
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-09-08 07:37:57 +0100

    feat: make game over dialog more retro

--- lib/pages/game_over.dart
+++ lib/pages/game_over.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nes_ui/nes_ui.dart';
 import 'package:ricochlime/flame/ricochlime_game.dart';
 import 'package:ricochlime/i18n/strings.g.dart';
 import 'package:ricochlime/utils/prefs.dart';
@@ -17,77 +18,81 @@ class GameOverDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return AlertDialog(
-      content: ConstrainedBox(
+    return Center(
+      child: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 400),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Text(
-                t.gameOverPage.title,
-                style: const TextStyle(
-                  fontSize: kToolbarHeight,
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-              child: Text.rich(
-                TextSpan(
-                  style: TextStyle(
-                    fontSize: kToolbarHeight / 2,
-                    color: colorScheme.onSurface,
+        child: NesContainer(
+          backgroundColor: colorScheme.surface,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  t.gameOverPage.title,
+                  style: const TextStyle(
+                    fontSize: kToolbarHeight,
                   ),
-                  children: [
-                    if (score > Prefs.highScore.value && Prefs.highScore.value > 0)
-                      t.gameOverPage.highScoreBeaten(
-                        pOld: TextSpan(
-                          style: TextStyle(
-                            decoration: TextDecoration.lineThrough,
-                            decorationThickness: kToolbarHeight / 20,
-                            decorationColor: colorScheme.onSurface.withOpacity(0.6),
-                          ),
-                          text: ' ${Prefs.highScore.value} ',
-                        ),
-                        pNew: TextSpan(
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: kToolbarHeight / 2,
-                          ),
-                          text: '$score',
-                        ),
-                      )
-                    else
-                      TextSpan(
-                        text: t.gameOverPage.highScoreNotBeaten(p: score),
-                      )
-                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 32),
-
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Column(children: [
-                _GameOverButton(
-                  onPressed: () {
-                    context.pop();
-                  },
-                  text: t.gameOverPage.playAgainButton,
+              SingleChildScrollView(
+                child: Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                      fontSize: kToolbarHeight / 2,
+                      color: colorScheme.onSurface,
+                    ),
+                    children: [
+                      if (score > Prefs.highScore.value && Prefs.highScore.value > 0)
+                        t.gameOverPage.highScoreBeaten(
+                          pOld: TextSpan(
+                            style: TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              decorationThickness: kToolbarHeight / 20,
+                              decorationColor: colorScheme.onSurface.withOpacity(0.6),
+                            ),
+                            text: ' ${Prefs.highScore.value} ',
+                          ),
+                          pNew: TextSpan(
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: kToolbarHeight / 2,
+                            ),
+                            text: '$score',
+                          ),
+                        )
+                      else
+                        TextSpan(
+                          text: t.gameOverPage.highScoreNotBeaten(p: score),
+                        )
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 32),
-                _GameOverButton(
-                  onPressed: () {
-                    context.pop(); // pop dialog
-                    context.pop(); // pop play page
-                  },
-                  text: t.gameOverPage.homeButton,
-                ),
-              ]),
-            ),
-          ],
+              ),
+              const SizedBox(height: 32),
+            
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Column(children: [
+                  _GameOverButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+                    type: NesButtonType.primary,
+                    text: t.gameOverPage.playAgainButton,
+                  ),
+                  const SizedBox(height: 32),
+                  _GameOverButton(
+                    onPressed: () {
+                      context.pop(); // pop dialog
+                      context.pop(); // pop play page
+                    },
+                    text: t.gameOverPage.homeButton,
+                  ),
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -99,26 +104,21 @@ class _GameOverButton extends StatelessWidget {
     // ignore: unused_element
     super.key,
     required this.onPressed,
+    this.type = NesButtonType.normal,
     required this.text,
   });
 
   final VoidCallback onPressed;
+  final NesButtonType type;
   final String text;
 
   @override
   Widget build(BuildContext context) {
     const double buttonSize = 28;
     return Center(
-      child: ElevatedButton(
+      child: NesButton(
         onPressed: onPressed,
-        style: ButtonStyle(
-          padding: MaterialStateProperty.all(
-            const EdgeInsets.symmetric(
-              horizontal: buttonSize,
-              vertical: buttonSize / 2,
-            ),
-          ),
-        ),
+        type: type,
         child: Text(
           text,
           softWrap: false,
