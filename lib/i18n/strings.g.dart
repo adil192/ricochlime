f40393537ee43b1edf55d8e487434a38277dfa92
Fontan030 <84931152+Fontan030@users.noreply.github.com>
2023-11-28 20:11:52 +0700

    i18n: Add Kazakh (#17)
    * Kazakh translation
    
    * Cyrillic Hyperlegible font
    
    * font licenses
    
    * i18n: apply strings
    
    * i18n: add kk

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -3,10 +3,10 @@
 /// Original: lib/i18n
 /// To regenerate, run: `dart run slang`
 ///
-/// Locales: 1
-/// Strings: 34
+/// Locales: 2
+/// Strings: 68 (34 per locale)
 ///
-/// Built on 2023-10-19 at 21:19 UTC
+/// Built on 2023-11-06 at 19:03 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -25,7 +25,8 @@ const AppLocale _baseLocale = AppLocale.en;
 /// - Locale locale = AppLocale.en.flutterLocale // get flutter locale from enum
 /// - if (LocaleSettings.currentLocale == AppLocale.en) // locale check
 enum AppLocale with BaseAppLocale<AppLocale, _StringsEn> {
-	en(languageCode: 'en', build: _StringsEn.build);
+	en(languageCode: 'en', build: _StringsEn.build),
+	kk(languageCode: 'kk', build: _StringsKk.build);
 
 	const AppLocale({required this.languageCode, this.scriptCode, this.countryCode, required this.build}); // ignore: unused_element
 
@@ -252,4 +253,137 @@ class _StringsCommonEn {
 	// Translations
 	String get cancel => 'Cancel';
 	String get ok => 'Okay';
+}
+
+// Path: <root>
+class _StringsKk extends _StringsEn {
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	_StringsKk.build({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = TranslationMetadata(
+		    locale: AppLocale.kk,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ),
+		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver);
+
+	/// Metadata for the translations of <kk>.
+	@override final TranslationMetadata<AppLocale, _StringsEn> $meta;
+
+	@override late final _StringsKk _root = this; // ignore: unused_field
+
+	// Translations
+	@override String get appName => 'Рикошырыш';
+	@override late final _StringsHomePageKk homePage = _StringsHomePageKk._(_root);
+	@override late final _StringsPlayPageKk playPage = _StringsPlayPageKk._(_root);
+	@override late final _StringsSettingsPageKk settingsPage = _StringsSettingsPageKk._(_root);
+	@override late final _StringsAgeDialogKk ageDialog = _StringsAgeDialogKk._(_root);
+	@override late final _StringsGameOverPageKk gameOverPage = _StringsGameOverPageKk._(_root);
+	@override late final _StringsTutorialPageKk tutorialPage = _StringsTutorialPageKk._(_root);
+	@override late final _StringsCommonKk common = _StringsCommonKk._(_root);
+}
+
+// Path: homePage
+class _StringsHomePageKk extends _StringsHomePageEn {
+	_StringsHomePageKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get playButton => 'Ойнау';
+	@override String get settingsButton => 'Баптау';
+	@override String get tutorialButton => 'Нұсқаулық';
+}
+
+// Path: playPage
+class _StringsPlayPageKk extends _StringsPlayPageEn {
+	_StringsPlayPageKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String highScore({required Object p}) => 'Үздік: ${p}';
+}
+
+// Path: settingsPage
+class _StringsSettingsPageKk extends _StringsSettingsPageEn {
+	_StringsSettingsPageKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Баптау';
+	@override String get adConsent => 'Жарнамаға келісімді баптау';
+	@override String get hyperlegibleFont => 'Atkinson Hyperlegible қарпін қолдану';
+	@override String get appInfo => 'Қолданба ақпары';
+	@override String licenseNotice({required Object buildYear}) => 'Ricochlime  Copyright (C) 2023-${buildYear}  Adil Hanney\nБұл бағдарлама еш кепілдіксіз жеткізіледі. Ол еркін екенін ескере отырып, сіз оны кейбір шарттардың аясында еркін тарата аласыз.';
+}
+
+// Path: ageDialog
+class _StringsAgeDialogKk extends _StringsAgeDialogEn {
+	_StringsAgeDialogKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get yourAge => 'Жасыңыз';
+	@override String get title => 'Жасыңызды болжау';
+	@override String get unknown => 'Белгісіз';
+	@override String get reason => 'Сіз көретін жарнама сізге қолайлы болатынына көз жеткізу үшін жасыңызды білуіміз керек. Ойынның өзіне бұл әсер етпейді.';
+	@override String areYou({required Object age}) => 'Жасыңыз ${age}?';
+	@override String get younger => 'Жоқ, жасым кішірек';
+	@override String get older => 'Жоқ, жасым үлкенірек';
+	@override String yesMyAgeIs({required Object age}) => 'Иә, жасым ${age}';
+	@override String get reset => 'Арылту';
+}
+
+// Path: gameOverPage
+class _StringsGameOverPageKk extends _StringsGameOverPageEn {
+	_StringsGameOverPageKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Ойын бітті!';
+	@override String highScoreNotBeaten({required Object p}) => '${p} ұпай жинадыңыз!';
+	@override TextSpan highScoreBeaten({required InlineSpan pOld, required InlineSpan pNew}) => TextSpan(children: [
+		const TextSpan(text: 'Енді үздік нәтижеңіз '),
+		pOld,
+		const TextSpan(text: ' '),
+		pNew,
+		const TextSpan(text: ' ұпай!'),
+	]);
+	@override String get continueWithAdButton => 'Жарнама көріп, жалғастыру';
+	@override String get restartGameButton => 'Жаңадан бастау';
+	@override String get homeButton => 'Мәзір';
+}
+
+// Path: tutorialPage
+class _StringsTutorialPageKk extends _StringsTutorialPageEn {
+	_StringsTutorialPageKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get tutorial => 'Нұсқаулық';
+	@override String get aimAtSlimes => 'Көздеу үшін саусағыңызды жылжытыңыз, ату үшін босатып жіберіңіз.';
+	@override String get emptyHealthbar => 'Шырышты жеңу үшін оның денсаулық жолағын нөлге дейін жеткізіңіз.';
+	@override String get bounceOffWalls => 'Қабырғадан ыршыған оқ көбірек шырышқа тие алады.';
+	@override String get tapSpeedUp => 'Оқ қозғалысын тездету үшін экранды түртіңіз.';
+	@override String get dangerZone => 'Қауіпті аймаққа кірген шырышты жеңбесеңіз, келесі жүрісте ұтыласыз.';
+	@override String get moreSlimes => 'Жүріс жасалған сайын жаңа шырыш қатары шығады. Ойын барысында бір жүрістен кейін пайда болатын қатарлар көбейіп, қауіпті аймақ та кеңейеді.';
+}
+
+// Path: common
+class _StringsCommonKk extends _StringsCommonEn {
+	_StringsCommonKk._(_StringsKk root) : this._root = root, super._(root);
+
+	@override final _StringsKk _root; // ignore: unused_field
+
+	// Translations
+	@override String get cancel => 'Болдырмау';
+	@override String get ok => 'ОК';
 }
