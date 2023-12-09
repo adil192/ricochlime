b0c0627df6ab68353c6c8e3bcc14ee6e011398e3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-09 22:07:00 +0000

    v1.1.0

--- installers/desktop_inno_script.iss
+++ installers/desktop_inno_script.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Ricochlime"
-#define MyAppVersion "1.0.6"
+#define MyAppVersion "1.1.0"
 #define MyAppPublisher "Adil Hanney"
 #define MyAppURL "https://github.com/adil192/ricochlime"
 #define MyAppExeName "ricochlime.exe"
--- scripts/apply_version.sh
+++ scripts/apply_version.sh
@@ -152,7 +152,7 @@ else
             </description>
         </release>\
 """
-  awk -v release="$RELEASE_TAG" 'NR==61{print release}1' "$FLATPAK_FILE" > "${FLATPAK_FILE}.tmp"
+  awk -v release="$RELEASE_TAG" 'NR==62{print release}1' "$FLATPAK_FILE" > "${FLATPAK_FILE}.tmp"
   mv "${FLATPAK_FILE}.tmp" "$FLATPAK_FILE"
 fi
 
