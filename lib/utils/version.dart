88a3820d5dac464c92af33ddb49a65650f8e6fd9
Adil Hanney <adilhanney@disroot.org>
2024-05-19 16:42:29 +0100

    v1.5.0

--- lib/utils/version.dart
+++ lib/utils/version.dart
@@ -2,10 +2,10 @@
 // Run `./scripts/apply_version.sh --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 104020;
+const int buildNumber = 105000;
 
 /// The current app version as a string.
-const String buildName = '1.4.2';
+const String buildName = '1.5.0';
 
 /// The year in which the current version was released.
 const int buildYear = 2024;
