df18c147e48a4d591795b7d76a1d5069a93918f2
Adil Hanney <adilhanney@disroot.org>
2024-05-31 20:57:16 +0100

    metadata: add aim guide to screenshots

--- test/game_golden_test.dart
+++ test/game_golden_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,7 +15,7 @@ import 'package:ricochlime/utils/ricochlime_palette.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('Golden game test', (tester) async {
+  group('Goldens', () {
     TestWidgetsFlutterBinding.ensureInitialized();
     SharedPreferences.setMockInitialValues({});
     Prefs.testingMode = true;
@@ -23,11 +24,13 @@ void main() {
 
     RicochlimeGame.disableBgMusic = true;
     RicochlimeGame.reproducibleGoldenMode = true;
-    game.random = Random(123);
-    await tester.runAsync(() => Future.wait([
-          game.preloadSprites.future,
-          GoogleFonts.pendingFonts([GoogleFonts.silkscreenTextTheme()]),
-        ]));
+    setUp(() async {
+      game.random = Random(123);
+      await Future.wait([
+        game.preloadSprites.future,
+        GoogleFonts.pendingFonts([GoogleFonts.silkscreenTextTheme()]),
+      ]);
+    });
 
     Prefs.coins.value = 166;
     Prefs.currentGame.value = GameData.fromJson(jsonDecode(
@@ -35,37 +38,54 @@ void main() {
         '{"score":50,"monsters":[{"px":0.0,"py":16.0,"maxHp":50,"killReward":1},{"px":32.0,"py":16.0,"maxHp":50},{"px":64.0,"py":16.0,"maxHp":50,"killReward":2},{"px":80.0,"py":16.0,"maxHp":50},{"px":32.0,"py":32.0,"maxHp":49,"killReward":1},{"px":96.0,"py":32.0,"maxHp":49,"killReward":2},{"px":32.0,"py":48.0,"maxHp":48,"killReward":2},{"px":80.0,"py":48.0,"maxHp":48,"killReward":1},{"px":16.0,"py":64.0,"maxHp":47,"killReward":2},{"px":80.0,"py":64.0,"maxHp":47,"killReward":1},{"px":80.0,"py":80.0,"maxHp":46,"killReward":1},{"px":112.0,"py":80.0,"maxHp":46,"killReward":2},{"px":16.0,"py":96.0,"maxHp":45,"killReward":1},{"px":96.0,"py":96.0,"maxHp":45,"killReward":2},{"px":0.0,"py":112.0,"maxHp":44},{"px":80.0,"py":112.0,"maxHp":44,"killReward":2},{"px":96.0,"py":112.0,"maxHp":44,"killReward":1},{"px":32.0,"py":128.26667199999997,"maxHp":43,"killReward":1},{"px":64.0,"py":128.26667199999997,"maxHp":43}]}'));
     Prefs.highScore.value = 62;
 
-    var widget = GameEnvironment(
-      game: game,
+    _testGame(
       screenSize: const Size(1440, 3120),
       pixelRatio: 10 / 3,
-    );
-    await tester.pumpWidget(widget);
-    await _precacheCoinImage(tester);
-    await tester.pumpFrames(widget, const Duration(seconds: 3));
-    await expectLater(
-      find.byType(PlayPage),
-      matchesGoldenFile('../metadata/en-US/images/phoneScreenshots/2_play.png'),
+      goldenFile: '../metadata/en-US/images/phoneScreenshots/2_play.png',
     );
 
-    widget = GameEnvironment(
-      game: game,
+    _testGame(
       screenSize: const Size(1440, 900),
       pixelRatio: 1,
-    );
-    await tester.pumpWidget(widget);
-    await tester.pumpFrames(widget, const Duration(seconds: 3));
-    await expectLater(
-      find.byType(PlayPage),
-      matchesGoldenFile('../metadata/en-US/images/tenInchScreenshots/game.png'),
+      goldenFile: '../metadata/en-US/images/tenInchScreenshots/game.png',
     );
   });
 }
 
-Future<void> _precacheCoinImage(WidgetTester tester) async {
-  final context = tester.element(find.byType(PlayPage));
-  await tester.runAsync(
-      () => precacheImage(const AssetImage('assets/images/coin.png'), context));
+void _testGame({
+  required Size screenSize,
+  required double pixelRatio,
+  required String goldenFile,
+}) {
+  testWidgets('$screenSize', (tester) async {
+    final widget = GameEnvironment(
+      game: game,
+      screenSize: screenSize,
+      pixelRatio: pixelRatio,
+    );
+    await tester.pumpWidget(widget);
+
+    final context = tester.element(find.byType(PlayPage));
+    await tester.runAsync(() =>
+        precacheImage(const AssetImage('assets/images/coin.png'), context));
+
+    // Aim towards the middle left of the game area
+    final aspectRatio = screenSize.width / screenSize.height;
+    if (aspectRatio < RicochlimeGame.aspectRatio || true) {
+      game.onPanUpdate(DragUpdateInfo.fromDetails(
+        game,
+        DragUpdateDetails(
+          globalPosition: const Offset(0, RicochlimeGame.expectedHeight * 0.39),
+        ),
+      ));
+    }
+
+    await tester.pumpFrames(widget, const Duration(seconds: 3));
+    await expectLater(
+      find.byType(PlayPage),
+      matchesGoldenFile(goldenFile),
+    );
+  });
 }
 
 class GameEnvironment extends StatelessWidget {
