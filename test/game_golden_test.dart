4586a2fd1fbc5c457afbf2f34d3cc86aeca9da3f
Adil Hanney <adilhanney@disroot.org>
2024-05-31 17:29:21 +0100

    test: use save file for goldens

--- test/game_golden_test.dart
+++ test/game_golden_test.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ricochlime/ads/ads.dart';
+import 'package:ricochlime/flame/game_data.dart';
 import 'package:ricochlime/flame/ricochlime_game.dart';
 import 'package:ricochlime/nes/nes_theme.dart';
 import 'package:ricochlime/pages/play.dart';
@@ -22,6 +24,12 @@ void main() {
     RicochlimeGame.reproducibleGoldenMode = true;
     game.random = Random(123);
     await tester.runAsync(() => game.preloadSprites.future);
+
+    Prefs.coins.value = 166;
+    Prefs.currentGame.value = GameData.fromJson(jsonDecode(
+        // ignore: lines_longer_than_80_chars
+        '{"score":50,"monsters":[{"px":0.0,"py":16.0,"maxHp":50,"killReward":1},{"px":32.0,"py":16.0,"maxHp":50},{"px":64.0,"py":16.0,"maxHp":50,"killReward":2},{"px":80.0,"py":16.0,"maxHp":50},{"px":32.0,"py":32.0,"maxHp":49,"killReward":1},{"px":96.0,"py":32.0,"maxHp":49,"killReward":2},{"px":32.0,"py":48.0,"maxHp":48,"killReward":2},{"px":80.0,"py":48.0,"maxHp":48,"killReward":1},{"px":16.0,"py":64.0,"maxHp":47,"killReward":2},{"px":80.0,"py":64.0,"maxHp":47,"killReward":1},{"px":80.0,"py":80.0,"maxHp":46,"killReward":1},{"px":112.0,"py":80.0,"maxHp":46,"killReward":2},{"px":16.0,"py":96.0,"maxHp":45,"killReward":1},{"px":96.0,"py":96.0,"maxHp":45,"killReward":2},{"px":0.0,"py":112.0,"maxHp":44},{"px":80.0,"py":112.0,"maxHp":44,"killReward":2},{"px":96.0,"py":112.0,"maxHp":44,"killReward":1},{"px":32.0,"py":128.26667199999997,"maxHp":43,"killReward":1},{"px":64.0,"py":128.26667199999997,"maxHp":43}]}'));
+    Prefs.highScore.value = 62;
 
     var widget = GameEnvironment(
       game: game,
