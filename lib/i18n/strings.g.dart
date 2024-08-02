39f84a5f45ca3f6ff806198ac8e1320a4a35544d
Yurt Page <yurtpage@gmail.com>
2024-08-02 05:48:59 +0300

    i18n: Add Russian (#50)

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -3,10 +3,10 @@
 /// Original: lib/i18n
 /// To regenerate, run: `dart run slang`
 ///
-/// Locales: 3
-/// Strings: 196 (65 per locale)
+/// Locales: 4
+/// Strings: 264 (66 per locale)
 ///
-/// Built on 2024-07-28 at 11:47 UTC
+/// Built on 2024-08-02 at 02:47 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -27,7 +27,8 @@ const AppLocale _baseLocale = AppLocale.en;
 enum AppLocale with BaseAppLocale<AppLocale, Translations> {
 	en(languageCode: 'en', build: Translations.build),
 	es(languageCode: 'es', build: _StringsEs.build),
-	kk(languageCode: 'kk', build: _StringsKk.build);
+	kk(languageCode: 'kk', build: _StringsKk.build),
+	ru(languageCode: 'ru', build: _StringsRu.build);
 
 	const AppLocale({required this.languageCode, this.scriptCode, this.countryCode, required this.build}); // ignore: unused_element
 
@@ -387,6 +388,7 @@ class _StringsSettingsPageEs extends _StringsSettingsPageEn {
 	@override final _StringsEs _root; // ignore: unused_field
 
 	// Translations
+	@override String get showReflectionInAimGuide => 'Mostrar reflejo en la guía de objetivos.';
 	@override String get ads => 'Anuncios';
 	@override String get hyperlegibleFont => 'Fuente fácil de leer';
 	@override String get biggerBullets => 'balas mas grandes';
@@ -588,6 +590,7 @@ class _StringsSettingsPageKk extends _StringsSettingsPageEn {
 	// Translations
 	@override String get title => 'Баптау';
 	@override String get adConsent => 'Жарнамаға келісімді баптау';
+	@override String get showReflectionInAimGuide => 'Мақсат нұсқаулығында рефлексияны көрсетіңіз';
 	@override String get appInfo => 'Қолданба ақпары';
 	@override String licenseNotice({required Object buildYear}) => 'Ricochlime  Copyright (C) 2023-${buildYear}  Adil Hanney\nБұл бағдарлама еш кепілдіксіз жеткізіледі. Ол еркін екенін ескере отырып, сіз оны кейбір шарттардың аясында еркін тарата аласыз.';
 	@override String get ads => 'Жарнамалар';
@@ -718,4 +721,204 @@ class _StringsCommonKk extends _StringsCommonEn {
 	// Translations
 	@override String get cancel => 'Болдырмау';
 	@override String get ok => 'ОК';
+}
+
+// Path: <root>
+class _StringsRu extends Translations {
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	_StringsRu.build({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = TranslationMetadata(
+		    locale: AppLocale.ru,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ),
+		  super.build(cardinalResolver: cardinalResolver, ordinalResolver: ordinalResolver);
+
+	/// Metadata for the translations of <ru>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	@override late final _StringsRu _root = this; // ignore: unused_field
+
+	// Translations
+	@override String get appName => 'Рикошлим';
+	@override late final _StringsHomePageRu homePage = _StringsHomePageRu._(_root);
+	@override late final _StringsPlayPageRu playPage = _StringsPlayPageRu._(_root);
+	@override late final _StringsSettingsPageRu settingsPage = _StringsSettingsPageRu._(_root);
+	@override late final _StringsAgeDialogRu ageDialog = _StringsAgeDialogRu._(_root);
+	@override late final _StringsGameOverPageRu gameOverPage = _StringsGameOverPageRu._(_root);
+	@override late final _StringsAdWarningRu adWarning = _StringsAdWarningRu._(_root);
+	@override late final _StringsRestartGameDialogRu restartGameDialog = _StringsRestartGameDialogRu._(_root);
+	@override late final _StringsTutorialPageRu tutorialPage = _StringsTutorialPageRu._(_root);
+	@override late final _StringsShopPageRu shopPage = _StringsShopPageRu._(_root);
+	@override late final _StringsCommonRu common = _StringsCommonRu._(_root);
+}
+
+// Path: homePage
+class _StringsHomePageRu extends _StringsHomePageEn {
+	_StringsHomePageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get playButton => 'Играть';
+	@override String get shopButton => 'Купить';
+	@override String get settingsButton => 'Настройки';
+	@override String get tutorialButton => 'Как играть';
+}
+
+// Path: playPage
+class _StringsPlayPageRu extends _StringsPlayPageEn {
+	_StringsPlayPageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String highScore({required Object p}) => 'Лучший: ${p}';
+	@override String get undo => 'Отменить бросок';
+	@override String get coins => 'Монетки';
+}
+
+// Path: settingsPage
+class _StringsSettingsPageRu extends _StringsSettingsPageEn {
+	_StringsSettingsPageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Настройки';
+	@override String get ads => 'Реклама';
+	@override String get gameplay => 'Игра';
+	@override String get accessibility => 'Доступность';
+	@override String get adConsent => 'Изменить согласие с рекламой';
+	@override String get hyperlegibleFont => 'Большой шрифт';
+	@override String get bgmVolume => 'Громкость фоновой музыки';
+	@override String get showUndoButton => 'Разрешить отмену бросков';
+	@override String get showReflectionInAimGuide => 'Показывать отскок в прицельной траектории';
+	@override String get fasterPageTransitions => 'Faster page transitions';
+	@override String get biggerBullets => 'Большие мячики';
+	@override String get maxFps => 'Максимальный FPS';
+	@override String get appInfo => 'О программе';
+	@override String licenseNotice({required Object buildYear}) => 'Ricochlime  Copyright (C) 2023-${buildYear}  Adil Hanney\nThis program comes with absolutely no warranty. This is free software, and you are welcome to redistribute it under certain conditions.';
+}
+
+// Path: ageDialog
+class _StringsAgeDialogRu extends _StringsAgeDialogEn {
+	_StringsAgeDialogRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get yourAge => 'Ваш возраст';
+	@override String get letMeGuessYourAge => 'Давайте-ка угадаю ваш возраст';
+	@override String get unknown => 'Неизвестно';
+	@override String get reason => 'Нам нужно знать ваш возраст чтобы знать какую рекламу показывать. Это не влияет на саму игру.';
+	@override String guessNumber({required Object n}) => 'Угадайте число \#${n}';
+	@override String areYou({required Object age}) => 'Вам ${age} лет?';
+	@override String get younger => 'Не, я моложе';
+	@override String get older => 'Нет, я старше';
+	@override String yesMyAgeIs({required Object age}) => 'Да, мне ${age} лет';
+	@override String get reset => 'Сбросить';
+	@override String get howOldAreYou => 'Сколько вам лет?';
+	@override String get invalidAge => 'Пожалуйста введите ваш возраст';
+	@override String get useMinigame => 'Поиграть в угадайку';
+	@override String get useSimpleInput => 'Ввести вручную';
+}
+
+// Path: gameOverPage
+class _StringsGameOverPageRu extends _StringsGameOverPageEn {
+	_StringsGameOverPageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Игра окончена!';
+	@override String highScoreNotBeaten({required Object p}) => 'Вы заработали ${p} очков!';
+	@override TextSpan highScoreBeaten({required InlineSpan pOld, required InlineSpan pNew}) => TextSpan(children: [
+		const TextSpan(text: 'Ваше наилучшее достижение теперь '),
+		pOld,
+		const TextSpan(text: ' '),
+		pNew,
+		const TextSpan(text: ' очков!'),
+	]);
+	@override String get continueWithCoins => 'Продолжить за 100';
+	@override String get restartGameButton => 'Перезапустить игру';
+	@override String get homeButton => 'На главную';
+}
+
+// Path: adWarning
+class _StringsAdWarningRu extends _StringsAdWarningEn {
+	_StringsAdWarningRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override TextSpan getCoins({required InlineSpan c, required InlineSpan t}) => TextSpan(children: [
+		const TextSpan(text: 'Получить '),
+		c,
+		const TextSpan(text: ' 100 после этой рекламы ('),
+		t,
+		const TextSpan(text: ')'),
+	]);
+}
+
+// Path: restartGameDialog
+class _StringsRestartGameDialogRu extends _StringsRestartGameDialogEn {
+	_StringsRestartGameDialogRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Перезапустить игру?';
+	@override String get areYouSure => 'Вы уверену что хотите начать снова? Вы не сможете вернуться';
+	@override String get waitCancel => 'Подождите, я передумал!';
+	@override String get yesImSure => 'Ну конечно!';
+}
+
+// Path: tutorialPage
+class _StringsTutorialPageRu extends _StringsTutorialPageEn {
+	_StringsTutorialPageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get tutorial => 'Как играть';
+	@override String get dragAndRelease => 'Defeat the monsters by dragging to aim and releasing to shoot.';
+	@override String get pointAndClick => 'Defeat the monsters by moving your mouse to aim and clicking to shoot.';
+	@override String get goldMonsters => 'Убив золотого монстра получаете монетку.';
+	@override String get greenMonsters => 'Убив зелёного монстра получаете дополнительный мячик.';
+	@override String get bounceOffWalls => 'Bounce your shots off the walls to hit the most monsters.';
+	@override String get tapSpeedUp => 'Прикоснитесь к экрану чтобы ускорить ваш выстрел.';
+	@override String get skullLine => 'A monster that reaches the skull line means game over if you don\'t defeat it in the next turn.';
+	@override String get moreMonsters => 'More rows of monsters will spawn each turn as you progress, so the skull line will move up.';
+	@override String get useCoinsInShop => 'Save up coins to unlock new items in the shop...';
+	@override String get orUseCoinsToContinue => '...or use them to continue after a game over.';
+}
+
+// Path: shopPage
+class _StringsShopPageRu extends _StringsShopPageEn {
+	_StringsShopPageRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Магазин';
+	@override String get bulletColors => 'Цвета мячика';
+	@override String get bulletShapes => 'Формы мячика';
+	@override String get premium => 'Премиум';
+	@override String get removeAdsForever => 'Удалить рекламу навсегда';
+	@override String get restorePurchases => 'Востановить покупки';
+}
+
+// Path: common
+class _StringsCommonRu extends _StringsCommonEn {
+	_StringsCommonRu._(_StringsRu root) : this._root = root, super._(root);
+
+	@override final _StringsRu _root; // ignore: unused_field
+
+	// Translations
+	@override String get cancel => 'Отмена';
+	@override String get ok => 'Окей';
 }
