738505e584c88b1e38ba90eb9036f8c353d2a585
Adil Hanney <adilhanney@disroot.org>
2025-01-12 22:07:32 +0000

    Chore: Remove AD_ID android permission

--- patches/foss.sh
+++ patches/foss.sh
@@ -4,8 +4,5 @@ sed -i -e '/in_app_purchase/d' pubspec.yaml
 echo "Replacing iaps with dummy class"
 mv lib/ads/_iap_foss.dart lib/ads/iap.dart
 
-echo "Removing AD_ID permission"
-sed -i -e '/com.google.android.gms.permission.AD_ID/d' android/app/src/main/AndroidManifest.xml
-
 echo "Removing internet permission"
 sed -i -e '/android.permission.INTERNET/d' android/app/src/main/AndroidManifest.xml
