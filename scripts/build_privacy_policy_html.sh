baaf32c307451012a69d87cf8b00187c7865a640
Adil Hanney <adilhanney@disroot.org>
2025-04-20 20:39:56 +0100

    Update build_privacy_policy_html.sh

--- scripts/build_privacy_policy_html.sh
+++ scripts/build_privacy_policy_html.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Converting markdown to html"
-HTML_CONTENT=$(pandoc -f markdown privacy_policy.md)
+pandoc -f markdown privacy_policy.md > temp.html
+
 echo "Inserting content into privacy_policy.html"
-sed -i "s|<privacy_policy_goes_here/>|$HTML_CONTENT|g" build/web/privacy_policy.html
+sed -i '/<privacy_policy_goes_here\/>/r temp.html' build/web/privacy_policy.html
+rm temp.html
