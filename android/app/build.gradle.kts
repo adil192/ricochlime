0aef15742622577b232145eef886a6849eda40f3
Adil Hanney <adilhanney@disroot.org>
2025-09-02 12:36:49 +0100

    fix: add back abiCodes for F-Droid
    https://github.com/adil192/ricochlime/issues/63#issuecomment-3243817701

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,3 +67,16 @@ flutter {
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
 }
+
+val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
+androidComponents {
+    onVariants { variant ->
+        variant.outputs.forEach { output ->
+            val abi = output.filters.find { it.filterType.name == "ABI" }?.identifier
+            val abiCode = abiCodes[abi]
+            if (abiCode != null) {
+                output.versionCode.set(flutter.versionCode * 10 + abiCode)
+            }
+        }
+    }
+}
