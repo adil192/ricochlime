6344b086014e2e32477f08b8ae27e0d1e24dc5c2
Adil Hanney <adilhanney@disroot.org>
2025-12-28 00:26:17 +0000

    v1.11.14

--- installers/desktop_inno_script.iss
+++ installers/desktop_inno_script.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Ricochlime"
-#define MyAppVersion "1.11.13"
+#define MyAppVersion "1.11.14"
 #define MyAppPublisher "Adil Hanney"
 #define MyAppURL "https://github.com/adil192/ricochlime"
 #define MyAppExeName "ricochlime.exe"
--- lib/utils/version.dart
+++ lib/utils/version.dart
@@ -2,10 +2,10 @@
 // Run `./scripts/apply_version.sh --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 111130;
+const int buildNumber = 111140;
 
 /// The current app version as a string.
-const String buildName = '1.11.13';
+const String buildName = '1.11.14';
 
 /// The year in which the current version was released.
 const int buildYear = 2025;
