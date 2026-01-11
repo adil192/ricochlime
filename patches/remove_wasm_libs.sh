cc0849996f4ca94a7975ef3276dc585045b01649
Adil Hanney <adilhanney@disroot.org>
2026-01-11 00:03:35 +0000

    patch: remove wasm libs on non-web

---
+++ patches/remove_wasm_libs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Patches the flutter_soloud package to remove precompiled WebAssembly libraries
+# since they're not needed for non-web platforms.
+# This patch will soon not be needed thanks to https://github.com/flutter/flutter/pull/176393.
+
+if [ ! -d ".dart_tool" ]; then
+  echo "Error: Run 'flutter pub get' before './patches/remove_wasm_libs.sh'"
+  exit 1
+fi
+
+# Find PUB_CACHE if not already set (https://dart.dev/tools/pub/environment-variables)
+if [ -z "$PUB_CACHE" ]; then
+  if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "win32"* ]]; then
+    PUB_CACHE="$LOCALAPPDATA/Pub/Cache"
+  else
+    PUB_CACHE="$HOME/.pub-cache"
+  fi
+fi
+echo "Using PUB_CACHE at: $PUB_CACHE"
+
+# Remove asset references in pubspec.yaml
+for pubspec_file in "$PUB_CACHE"/hosted/pub.dev/flutter_soloud-*/pubspec.yaml; do
+  if [ ! -f "$pubspec_file" ]; then
+    echo "Warning: pubspec.yaml not found in $pubspec_file"
+    continue
+  fi
+  echo "Patching $pubspec_file"
+
+  # pubspec.yaml contains assets like `web/*.wasm` and `web/*.js`
+  # Remove lines containing `web/*.wasm` and `web/*.js`
+  sed -i.bak -E '/^[[:space:]]*-[[:space:]]*web\/.*\.(js|wasm)[[:space:]]*$/d' "$pubspec_file"
+done
+
+for dir in "$PUB_CACHE"/hosted/pub.dev/flutter_soloud-*/web; do
+  if [ ! -d "$dir" ]; then
+    echo "Warning: web directory not found in $dir"
+    continue
+  fi
+  echo "Removing WebAssembly files in $dir"
+
+  # Remove all .wasm and .js files in the web directory
+  rm -vf "$dir"/*.wasm
+  rm -vf "$dir"/*.js
+done
+
+echo "Done!"
